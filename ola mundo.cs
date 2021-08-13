453cc3a2bf365605b32afad690ae754782f3909b
Felipealvesnn <88728350+Felipealvesnn@users.noreply.github.com>
2021-08-13 12:56:30 -0300

    Add files via upload

---
+++ ola mundo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace helloworld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola munndo");
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
